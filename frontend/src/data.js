82b1e0a5a2613acf730efe341169b6c7d39dc4a6
Manish Karki <manish.karki33@gmail.com>
2021-10-05 07:20:12 +0545

    Added

---
+++ frontend/src/data.js
@@ -0,0 +1,61 @@
+const data = {
+    products:[
+        {
+            _id: 1,
+            name: 'Nike Slik Shirt',
+            category: 'Shirt',
+            image: '/images/p1.jpg',
+            price: 1200,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High Quality Product'
+        },
+        {
+            _id: 2,
+            name: 'Adidas Fit Shirt',
+            category: 'Shirt',
+            image: '/images/p1.jpg',
+            price: 1000,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 20,
+            description: 'High Quality Product'
+        },
+        {
+            _id: 3,
+            name: 'Lacoste Free Shirt',
+            category: 'Shirt',
+            image: '/images/p1.jpg',
+            price: 1100,
+            brand: 'Lacoste',
+            rating: 4.6,
+            numReviews: 10,
+            description: 'High Quality Product'
+        },
+        {
+            _id: 4,
+            name: 'Nike Slik Pant',
+            category: 'Pant',
+            image: '/images/p1.jpg',
+            price: 1400,
+            brand: 'Nike',
+            rating: 3.8,
+            numReviews: 14,
+            description: 'High Quality Product'
+        },
+        {
+            _id: 5,
+            name: 'Puma Slik Pant',
+            category: 'Pant',
+            image: '/images/p1.jpg',
+            price: 1500,
+            brand: 'Puma',
+            rating: 4.3,
+            numReviews: 12,
+            description: 'High Quality Product'
+        },
+    ],
+}
+
+export default data;
